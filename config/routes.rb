8ccccdac4c2c46f028ffe3c124e8e2825cdc8406
Mike Murphy <mikeymurph77@gmail.com>
2014-06-16 17:29:16 -0400

    Add resounced to simplify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get "/" => "galleries#index"
-  get "galleries/new" => "galleries#new"
-  get "/galleries/:id" => "galleries#show"
-  post "/galleries" => "galleries#create"
-  get "/galleries/:id/edit" => "galleries#edit"
-  patch "/galleries/:id" => "galleries#update"
-  delete "/galleries/:id" => "galleries#destroy"
+  root to:"galleries#index"
+  resources :galleries
 end
