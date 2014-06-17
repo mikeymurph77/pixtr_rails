f6b0875a9eca6e86eec67e968609f6e60ae1699a
Mike Murphy <mikeymurph77@gmail.com>
2014-06-17 16:00:25 -0400

    Add edit image and delete buttons

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,11 +4,32 @@ class ImagesController < ApplicationController
 		@image = Image.new
 	end
 
+	def edit
+		@gallery = Gallery.find(params[:gallery_id])
+		@image = @gallery.images.find(params[:id])
+	end
+
+	def update
+		gallery = Gallery.find(params[:gallery_id])
+		image = gallery.images.find(params[:id])
+		image.update(image_params)
+
+		redirect_to gallery
+	end
+
 	def create
 		image = Image.create(image_params) #3
 		gallery = Gallery.find(params[:gallery_id]) #2
 
 		redirect_to "/galleries/#{gallery.id}" #1
+	end
+
+	def destroy
+		gallery = Gallery.find(params[:gallery_id])
+		image = gallery.images.find(params[:id])
+		
+		image.destroy	
+		redirect_to gallery	
 	end
 
 	private
