0e6a827a23bbf214363a8b005743a353e3ff0672
Mike Murphy <mikeymurph77@gmail.com>
2014-06-18 11:56:04 -0400

    Add render files to new application directory

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,24 +4,31 @@ class ImagesController < ApplicationController
 		@image = Image.new
 	end
 
+	def create
+		@gallery = Gallery.find(params[:gallery_id])
+		@image = Image.new(image_params)
+
+		if @image.save
+			redirect_to @gallery
+		else
+			render :new
+		end
+	end
+
 	def edit
 		@gallery = Gallery.find(params[:gallery_id])
 		@image = @gallery.images.find(params[:id])
 	end
 
 	def update
-		gallery = Gallery.find(params[:gallery_id])
-		image = gallery.images.find(params[:id])
-		image.update(image_params)
-
-		redirect_to gallery
-	end
-
-	def create
-		image = Image.create(image_params) #3
-		gallery = Gallery.find(params[:gallery_id]) #2
-
-		redirect_to "/galleries/#{gallery.id}" #1
+		@gallery = Gallery.find(params[:gallery_id])
+		@image = @gallery.images.find(params[:id])
+		
+		if @image.update(image_params)
+			redirect_to @gallery
+		else
+			render :edit
+		end
 	end
 
 	def destroy
