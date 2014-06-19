ed9b5cc162cded4d512640c92c1346d794aa5dbe
Mike Murphy <mikeymurph77@gmail.com>
2014-06-19 16:44:24 -0400

    Add the before_action to require login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # UMBRELLA OF PROTECTION
+  before_action :require_login
 end
