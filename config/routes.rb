eb3b85b68bcc400438bb2430a8f1ae306c478b5a
Mike Murphy <mikeymurph77@gmail.com>
2014-06-19 11:06:31 -0400

    Add ability to sign up and sign in with Monban

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
   root to:"galleries#index"
   resources :galleries, only: [:show, :new, :create, :edit, :update, :destroy] do
     resources :images, only: [:new, :create, :edit, :update, :destroy]
